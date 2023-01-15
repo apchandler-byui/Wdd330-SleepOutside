8631634fa66442499bc94f18cdaec6a30f633f75
Andrew C <apchandler11@gmail.com>
2023-01-15 03:03:04 -0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,28 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import json  from "@rollup/plugin-json";
 
 export default defineConfig({
   root: "src/",
-
+  assetsInclude: ["/json/*.json"],
   build: {
     outDir: "../dist",
     minify : false,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html"
-        ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html"
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html"
-        ),
-      },
+        input: {
+          main: resolve(__dirname, "src/index.html"),
+          cart: resolve(__dirname, "src/cart/index.html"),
+          checkout: resolve(__dirname, "src/checkout/index.html"),
+          product1: resolve(__dirname,"src/product_pages/cedar-ridge-rimrock-2.html"),
+          product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
+          product3: resolve(__dirname, "src/product_pages/northface-alpine-3.html"),
+          product4: resolve(__dirname,"src/product_pages/northface-talus-4.html"), 
+        },
+        output: {
+          assetFileNames: "assets/[name][extname]",
+          chunkFileNames: "assets/[name].mjs",
+          entryFileNames: "assets/[name].js"
+        },
     },
   },
 });
